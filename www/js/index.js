379f05b99d38b0481dc1a761155fb75d271bbf45
Lana-chan <erin.pinheiro@usp.br>
2016-10-18 18:52:33 -0200

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,43 @@
+
+var app = {
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+    
+    var canvas = document.getElementById('tutorial');
+
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+      // drawing code here
+    } else {
+      // canvas-unsupported code here
+    }
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  }
+};
+
+app.initialize();
