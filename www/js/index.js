0bc009b7589054ba179a71aa2f76cc4988149730
Lana-chan <erin.pinheiro@usp.br>
2016-10-20 19:25:26 -0200

    serial para texto

--- www/js/index.js
+++ www/js/index.js
@@ -24,6 +24,8 @@ var app = {
     //page transition fade é feia
     $.mobile.defaultPageTransition = "slide";
 
+    broche.initializeAnim();
+    draw.initialize();
   },
   // Bind Event Listeners
   //
@@ -49,6 +51,86 @@ var app = {
     receivedElement.setAttribute('style', 'display:block;');
 
     console.log('Received Event: ' + id);
+  }
+};
+
+// brocheDraw
+
+var broche = {
+  anim: [],
+  
+  // monta mensagem para envio serial
+  msg: function() {    
+    msg = "T" + $("#text-msg").value;
+    if($("#text-wrap").is(":checked")) msg = msg + " ... ";
+    return msg;
+  },
+  
+  // inicializa matriz 2d * págs para guardar animação
+  initializeAnim: function() {
+    
+  },
+  
+  // função callback de falha na serial  
+  serialFail: function() {
+    alert("Não foi possível estabelecer comunicação serial!");
+  },
+  
+  // função callback de sucesso ao conectar serial para subir
+  upSuccess: function() {
+    serial.open({},
+      function() {
+        if($("#xfer-anim").is(":checked")) {
+        
+        }
+        if($("#xfer-text").is(":checked")) {
+          serial.write(msg);
+          serial.writeHex('10',
+            function succes() {
+              alert("Sucesso!");
+              serial.write("SQ");
+            },
+            this.serialFail
+          );
+        }
+      },
+      this.serialFail
+    );
+  },
+  
+  // função chamada ao clicar subir
+  upload: function() {
+    if($("#xfer-anim").is(":checked") == false && $("#xfer-text").is(":checked") == false) {
+      alert("Não há nada selecionado para subir!");
+    } else {
+      serial.requestPermission(this.upSuccess, this.serialFail);
+    }
+  },
+  
+  // função chamada ao clicar baixar
+  download: function() {
+    if($("#xfer-anim").is(":checked") == false && $("#xfer-text").is(":checked") == false) {
+      alert("Não há nada selecionado para baixar!");
+    } else {
+    
+    }
+  },
+  
+  // atualiza animação ao mudar widget
+  updateFromWidgets: function() { 
+  },
+  
+  // atualiza widget ao carregar nova animação
+  updateToWidgets: function() {
+  }
+};
+
+var draw = {
+  initialize: function() {
+    console.log("ogij");
+    $("#screen-table").on("vmousedown", function() {
+      console.log("asdfg");
+    });
   }
 };
 
