f823f63db09a66a70329e1d39a0cc7a72d0f0a58
Lana-chan <erin.pinheiro@usp.br>
2016-10-21 01:37:30 -0200

    tweaks cordova para iniciar mais cedo

--- www/js/index.js
+++ www/js/index.js
@@ -3,56 +3,18 @@ $(function() {
   FastClick.attach(document.body);
 });
 
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {
+  
+}
+
 // external panels
 $(function () {
-    //$("[data-role=header],[data-role=footer]").toolbar().enhanceWithin();
-    $("[data-role=panel]").panel().enhanceWithin();
+  //$("[data-role=header],[data-role=footer]").toolbar().enhanceWithin();
+  $("[data-role=panel]").panel().enhanceWithin();
+  broche.initializeAnim();
+  draw.initialize();
 });
-
-/*$(document).on("pagecreate", function () {
-    $("[data-role=panel]").one("panelbeforeopen", function () {
-        var height = $.mobile.pageContainer.pagecontainer("getActivePage").outerHeight();
-        $(".ui-panel-wrapper").css("height", height + 1);
-    });
-});*/
-
-var app = {
-  // Application Constructor
-  initialize: function() {
-    this.bindEvents();
-    
-    //page transition fade é feia
-    $.mobile.defaultPageTransition = "slide";
-
-    broche.initializeAnim();
-    draw.initialize();
-  },
-  // Bind Event Listeners
-  //
-  // Bind any events that are required on startup. Common events are:
-  // 'load', 'deviceready', 'offline', and 'online'.
-  bindEvents: function() {
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-  },
-  // deviceready Event Handler
-  //
-  // The scope of 'this' is the event. In order to call the 'receivedEvent'
-  // function, we must explicitly call 'app.receivedEvent(...);'
-  onDeviceReady: function() {
-    app.receivedEvent('deviceready');
-  },
-  // Update DOM on a Received Event
-  receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
-  }
-};
 
 // brocheDraw
 
@@ -78,17 +40,21 @@ var broche = {
   
   // função callback de sucesso ao conectar serial para subir
   upSuccess: function() {
-    serial.open({},
+    serial.open({baudRate: 9600},
       function() {
         if($("#xfer-anim").is(":checked")) {
         
         }
         if($("#xfer-text").is(":checked")) {
-          serial.write(msg);
-          serial.writeHex('10',
-            function succes() {
-              alert("Sucesso!");
-              serial.write("SQ");
+          serial.write(this.msg,
+            function() {
+              serial.writeHex('10',
+                function succes() {
+                  alert("Sucesso!");
+                  serial.write("SQ");
+                },
+                this.serialFail
+              );
             },
             this.serialFail
           );
@@ -133,5 +99,3 @@ var draw = {
     });
   }
 };
-
-app.initialize();
