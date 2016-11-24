b8e351bbabf9d3646f6d234412c5a43db53141b7
Lana-chan <erin.manal@gmail.com>
2016-11-24 17:10:04 -0200

    conserta testes para remoção de form

--- tests/tests.js
+++ tests/tests.js
@@ -5,20 +5,14 @@ casper.test.on("fail", function () {
 casper.test.begin("Atualização de valor máximo de quadros", 2, function(test) {
   casper.start('www/index.html', function() {
     this.echo("Mudando número de quadros para 6");
-    this.fill("form#anim-form", {
-      "anim-frame-count": 6
-    }, false);
+    this.sendKeys('input[id="anim-frame-count"]', 6);
     var value = this.getElementAttribute('#anim-frame', 'max');
     test.assertEquals(value, "6", "Quadro máximo é 6");
     
     this.echo("Mudando quadro para 6");
-    this.fill("form#anim-form", {
-      "anim-frame": 6
-    }, false);
+    this.sendKeys('input[id="anim-frame"]', 6);
     this.echo("Mudando número de quadros para 4");
-    this.fill("form#anim-form", {
-      "anim-frame-count": 4
-    }, false);
+    this.sendKeys('input[id="anim-frame-count"]', 4);
     test.assertFieldCSS("#anim-frame", "4", "Quadro atual é 4");
   });
   
