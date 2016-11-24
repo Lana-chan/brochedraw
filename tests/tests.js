69385930b0fdeb7ef74f034f19c50e8c62cb498b
Lana-chan <erin.manal@gmail.com>
2016-11-24 17:39:45 -0200

    reescrevendo teste com jquery

--- tests/tests.js
+++ tests/tests.js
@@ -2,17 +2,21 @@ casper.test.on("fail", function () {
     casper.exit(1);
 });
 
+function changeVal(selector, value) {
+  $(selector).val(value).change();
+}
+
 casper.test.begin("Atualização de valor máximo de quadros", 2, function(test) {
   casper.start('www/index.html', function() {
     this.echo("Mudando número de quadros para 6");
-    this.sendKeys('input[id="anim-frame-count"]', 6);
+    this.evaluate(changeVal, '#anim-frame-count', 6);
     var value = this.getElementAttribute('#anim-frame', 'max');
     test.assertEquals(value, "6", "Quadro máximo é 6");
     
     this.echo("Mudando quadro para 6");
-    this.sendKeys('input[id="anim-frame"]', 6);
+    this.evaluate(changeVal, '#anim-frame', 6);
     this.echo("Mudando número de quadros para 4");
-    this.sendKeys('input[id="anim-frame-count"]', 4);
+    this.evaluate(changeVal, '#anim-frame-count', 4);
     test.assertFieldCSS("#anim-frame", "4", "Quadro atual é 4");
   });
   
